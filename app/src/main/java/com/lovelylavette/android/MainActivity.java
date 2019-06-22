8370c97ebd03dee3df6d84a63030f5bc92a75811
LaVette Clifton <lavette.clifton@gmail.com>
2019-06-22 14:06:09 -0500

    Add search options

--- app/src/main/java/com/lovelylavette/android/MainActivity.java
+++ app/src/main/java/com/lovelylavette/android/MainActivity.java
@@ -3,6 +3,9 @@ package com.lovelylavette.android;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -11,20 +14,28 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.nav_view) BottomNavigationView navView;
-    @BindView(R.id.message) TextView messageText;
+    @BindView(R.id.nav_view)
+    BottomNavigationView navView;
+    @BindView(R.id.message)
+    TextView messageText;
+
+    FragmentManager fragmentManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.menu_dashboard:
                     messageText.setText(R.string.title_dashboard);
                     return true;
                 case R.id.menu_search:
                     messageText.setText(R.string.title_search);
+                    ft.replace(R.id.frag_container, new SearchFragment());
+                    ft.commit();
                     return true;
                 case R.id.menu_trips:
                     messageText.setText(R.string.title_trips);
@@ -37,12 +48,22 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private void updateNav() {
+        Fragment fragment = fragmentManager.findFragmentById(R.id.frag_container);
+        if(fragment == null) {
+            navView.setSelectedItemId(R.id.menu_dashboard);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fragmentManager = getSupportFragmentManager();
+        updateNav();
     }
 
 }
---
+++ app/src/main/java/com/lovelylavette/android/SearchFragment.java
@@ -0,0 +1,24 @@
+package com.lovelylavette.android;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+
+public class SearchFragment extends Fragment {
+    private static final String TAG = "SearchFragment";
+
+    public SearchFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+}

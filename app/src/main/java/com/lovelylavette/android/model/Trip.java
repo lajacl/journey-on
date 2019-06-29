32731594f6b913ae8c179f0ce01c745265cd1304
LaVette Clifton <lavette.clifton@gmail.com>
2019-06-29 15:39:17 -0500

    Create trip model

---
+++ app/src/main/java/com/lovelylavette/android/model/Trip.java
@@ -0,0 +1,33 @@
+package com.lovelylavette.android.model;
+
+import com.amadeus.resources.FlightOffer;
+import com.amadeus.resources.HotelOffer;
+import com.amadeus.resources.PointOfInterest;
+import com.google.android.libraries.places.api.model.Place;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class Trip {
+    private UUID id;
+    private boolean flightNeeded;
+    private boolean hotelNeeded;
+    private boolean sightsNeeded;
+    private int budget;
+    private Place origin;
+    private Place destination;
+    private int numPlaneSeats;
+    private boolean roudtrip;
+    private Date departureDate;
+    private Date returnDate;
+    private int numHotelGuests;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private FlightOffer flight;
+    private HotelOffer hotel;
+    private List<PointOfInterest> sights;
+}

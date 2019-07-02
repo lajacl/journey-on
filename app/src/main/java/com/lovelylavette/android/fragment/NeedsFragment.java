0ef60b06b4b86bd2933eb9cdcfe375c232eb3900
LaVette Clifton <lavette.clifton@gmail.com>
2019-07-01 23:36:17 -0500

    Create trip needs fragment

---
+++ app/src/main/java/com/lovelylavette/android/fragment/NeedsFragment.java
@@ -0,0 +1,24 @@
+package com.lovelylavette.android.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lovelylavette.android.R;
+
+public class NeedsFragment extends Fragment {
+
+
+    public NeedsFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_needs, container, false);
+    }
+
+}

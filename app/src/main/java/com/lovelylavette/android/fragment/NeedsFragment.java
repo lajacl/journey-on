ab3064c693eccce5c39d40e30adf16cd5088a6a1
LaVette Clifton <lavette.clifton@gmail.com>
2019-07-03 14:42:32 -0500

    Update trip with selected needs

--- app/src/main/java/com/lovelylavette/android/fragment/NeedsFragment.java
+++ app/src/main/java/com/lovelylavette/android/fragment/NeedsFragment.java
@@ -3,17 +3,30 @@ package com.lovelylavette.android.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.lovelylavette.android.R;
 import com.lovelylavette.android.model.Trip;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NeedsFragment extends Fragment {
     private static final String TAG = "NeedsFragment";
     private static final String ARG_TRIP = "trip";
     private Trip trip;
+
+    @BindView(R.id.flight_cb)
+    CheckBox flightCheckBox;
+    @BindView(R.id.hotel_cb)
+    CheckBox hotelCheckBox;
+    @BindView(R.id.sights_cb)
+    CheckBox sightsCheckBox;
 
 
     public NeedsFragment() {
@@ -37,6 +50,30 @@ public class NeedsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_needs, container, false);
+        View view = inflater.inflate(R.layout.fragment_needs, container, false);
+        ButterKnife.bind(this, view);
+        addCheckedChangeListeners();
+        return view;
+    }
+
+    private void addCheckedChangeListeners() {
+        CompoundButton.OnCheckedChangeListener checkedChangeListener = (buttonView, isChecked) -> {
+            switch (buttonView.getId()) {
+                case R.id.flight_cb:
+                    trip.setFlightNeeded(isChecked);
+                    break;
+                case R.id.hotel_cb:
+                    trip.setHotelNeeded(isChecked);
+                    break;
+                case R.id.sights_cb:
+                    trip.setSightsNeeded(isChecked);
+                    break;
+            }
+            Log.i(TAG, trip.toString());
+        };
+
+        flightCheckBox.setOnCheckedChangeListener(checkedChangeListener);
+        hotelCheckBox.setOnCheckedChangeListener(checkedChangeListener);
+        sightsCheckBox.setOnCheckedChangeListener(checkedChangeListener);
     }
 }

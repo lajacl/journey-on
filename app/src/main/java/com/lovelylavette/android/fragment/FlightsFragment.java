c60e88b2a2cd105dd6f8ff97e9b9eee9b7ab334e
LaVette Clifton <lavette.clifton@gmail.com>
2019-07-04 11:01:27 -0400

    Create flights fragment

---
+++ app/src/main/java/com/lovelylavette/android/fragment/FlightsFragment.java
@@ -0,0 +1,21 @@
+package com.lovelylavette.android.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lovelylavette.android.R;
+
+public class FlightsFragment extends Fragment {
+
+    public FlightsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_flights, container, false);
+    }
+}

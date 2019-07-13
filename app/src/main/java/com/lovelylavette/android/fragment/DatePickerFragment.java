51d8fa8828cdd3a3c86522527bda8eb122d81473
LaVette Clifton <lavette.clifton@gmail.com>
2019-07-12 23:10:44 -0500

    Add multi-select calendar library

--- app/src/main/java/com/lovelylavette/android/fragment/DatePickerFragment.java
+++ app/src/main/java/com/lovelylavette/android/fragment/DatePickerFragment.java
@@ -8,9 +8,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.lovelylavette.android.R;
+import com.squareup.timessquare.CalendarPickerView;
+
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.squareup.timessquare.CalendarPickerView.SelectionMode.RANGE;
 
 public class DatePickerFragment extends Fragment {
     private static final String TAG = "DatePickerFragment";
+
+    @BindView(R.id.calendar_picker)
+    CalendarPickerView calendarPicker;
 
 
     public DatePickerFragment() {
@@ -20,6 +31,23 @@ public class DatePickerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_date_picker, container, false);
+        View view = inflater.inflate(R.layout.fragment_date_picker, container, false);
+        ButterKnife.bind(this, view);
+        setupCalendarPicker();
+        return view;
+    }
+
+    private void setupCalendarPicker() {
+        Calendar today = Calendar.getInstance();
+        Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 1);
+        nextYear.add(Calendar.MONTH, -1);
+        nextYear.add(Calendar.DAY_OF_MONTH, -4);
+
+        calendarPicker.init(today.getTime(), nextYear.getTime())
+                .inMode(RANGE);
+
+        calendarPicker.setOnInvalidDateSelectedListener(date -> {
+        });
     }
 }

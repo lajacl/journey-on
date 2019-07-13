4e09ad7e34250d120c01f97c290bd86026b9c5f8
LaVette Clifton <lavette.clifton@gmail.com>
2019-07-13 13:19:08 -0500

    Update date selection on flight type change

--- app/src/main/java/com/lovelylavette/android/fragment/DatePickerFragment.java
+++ app/src/main/java/com/lovelylavette/android/fragment/DatePickerFragment.java
@@ -3,6 +3,7 @@ package com.lovelylavette.android.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import com.squareup.timessquare.CalendarPickerView;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,8 +24,10 @@ import static com.squareup.timessquare.CalendarPickerView.SelectionMode.SINGLE;
 
 public class DatePickerFragment extends Fragment {
     private static final String TAG = "DatePickerFragment";
-    private Calendar startDate;
-    private Calendar endDate;
+    private Calendar today = Calendar.getInstance();
+    private Calendar nextYear = Calendar.getInstance();
+    private Calendar startDate = Calendar.getInstance();
+    private Calendar endDate = Calendar.getInstance();
 
     @BindView(R.id.calendar_picker)
     CalendarPickerView calendarPicker;
@@ -47,17 +51,16 @@ public class DatePickerFragment extends Fragment {
     }
 
     private void setupCalendar() {
-        startDate = Calendar.getInstance();
-        endDate = Calendar.getInstance();
-        endDate.add(Calendar.YEAR, 1);
-        endDate.add(Calendar.MONTH, -1);
-        endDate.add(Calendar.DAY_OF_MONTH, -4);
+        nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 1);
+        nextYear.add(Calendar.MONTH, -1);
+        nextYear.add(Calendar.DAY_OF_MONTH, -4);
 
         calendarInit(RANGE);
     }
 
     private void calendarInit(CalendarPickerView.SelectionMode mode) {
-        calendarPicker.init(startDate.getTime(), endDate.getTime())
+        calendarPicker.init(today.getTime(), nextYear.getTime())
                 .inMode(mode);
     }
 
@@ -70,6 +73,11 @@ public class DatePickerFragment extends Fragment {
         calendarPicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
             @Override
             public void onDateSelected(Date date) {
+                List<Date> selectedDates = calendarPicker.getSelectedDates();
+                startDate.setTime(selectedDates.get(0));
+                endDate.setTime(selectedDates.get(selectedDates.size() - 1));
+                Log.i(TAG, "Start Date: " + (startDate.get(Calendar.MONTH) + 1) + " - " + startDate.get(Calendar.DAY_OF_MONTH));
+                Log.i(TAG, "End Date: " + (endDate.get(Calendar.MONTH) + 1) + " - " + endDate.get(Calendar.DAY_OF_MONTH));
             }
 
             @Override
@@ -83,11 +91,15 @@ public class DatePickerFragment extends Fragment {
             switch (checkedId) {
                 case R.id.round_trip_radio:
                     calendarInit(RANGE);
+                    calendarPicker.selectDate(startDate.getTime());
+                    calendarPicker.selectDate(endDate.getTime());
                     break;
                 case R.id.one_way_radio:
                     calendarInit(SINGLE);
+                    calendarPicker.selectDate(startDate.getTime());
                     break;
             }
         });
     }
+
 }

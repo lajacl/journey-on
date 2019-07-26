2210554880264260901f8d4166a3455a5cd00947
LaVette Clifton <lavette.clifton@gmail.com>
2019-07-25 20:45:28 -0500

    Add user name to header

--- app/src/main/java/com/lovelylavette/android/fragment/DestinationFragment.java
+++ app/src/main/java/com/lovelylavette/android/fragment/DestinationFragment.java
@@ -11,12 +11,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.amadeus.resources.FlightDestination;
 import com.lovelylavette.android.R;
 import com.lovelylavette.android.adapter.DestinationAdapter;
 import com.lovelylavette.android.api.AmadeusApi;
+import com.lovelylavette.android.model.User;
+import com.lovelylavette.android.util.AppPrefs;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,8 +27,12 @@ import butterknife.ButterKnife;
 public class DestinationFragment extends Fragment {
     private static final String TAG = "InspirationFragment";
     private Context context;
+    private AppPrefs prefs;
+    private User user;
     private DestinationAdapter destinationAdapter;
 
+    @BindView(R.id.destination_header)
+    TextView header;
     @BindView(R.id.progress_bar)
     ProgressBar progressBar;
     @BindView(R.id.destination_recycler)
@@ -41,9 +48,18 @@ public class DestinationFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_destination, container, false);
         ButterKnife.bind(this, view);
+        showUserName();
         setupRecycler();
         getDestinations();
         return view;
+    }
+
+    private void showUserName() {
+        prefs = new AppPrefs(context);
+        user = prefs.getUser();
+        if (user != null && user.getName() != null && !user.getName().isEmpty()) {
+            header.setText(String.format("Hello %s,\n%s", user.getName(), header.getText()));
+        }
     }
 
     private void setupRecycler() {
